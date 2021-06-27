6a51c758e1ba8e9fffe47e45257b8f5d8c3c48f0
Lucas Massignani <lucasmassignani@gmail.com>
2021-06-27 19:27:29 -0300

    Feito: Reconhecimento IGMC e Filtro de pacotes

---
+++ PraisedSniffer/IGMPHeader.cs
@@ -0,0 +1,48 @@
+﻿using PacketDotNet;
+using System.Text;
+
+namespace PraisedSniffer
+{
+    public class IGMPHeader
+    {
+        private IGMPv2Packet _igmpv2Packet;
+
+        public IGMPHeader(IGMPv2Packet igmpv2Packet)
+        {
+            _igmpv2Packet = igmpv2Packet;
+        }
+
+        public string Checksum
+        {
+            get
+            {
+                return string.Format("0x{0:x2}", _igmpv2Packet.Checksum);
+            }
+        }
+
+        public string MaxResponseTime
+        {
+            get
+            {
+                return _igmpv2Packet.MaxResponseTime.ToString();
+            }
+        }
+
+        public string GroupAddress
+        {
+            get
+            {
+                return _igmpv2Packet.GroupAddress.ToString();
+            }
+        }
+
+
+        public string Tipo
+        {
+            get
+            {
+                return _igmpv2Packet.Type.ToString();
+            }
+        }
+    }
+}
